332da0e0a710874f6496ee4ede09e5ba63d1f7e5
Kenshiro <>
2023-05-03 23:44:30 +0200

    Text color

--- magi.py
+++ magi.py
@@ -5,6 +5,10 @@ import sys
 MOD_FILE_PATH = "mod.txt"
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+SYSTEM_COLOR = "\033[91m"
+MAGI_COLOR = "\033[99m"
+USER_COLOR = "\033[93m"
+END_COLOR = "\x1b[0m"
 
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
 
@@ -30,16 +34,33 @@ def send_prompt(mod, prompt, context):
 	return response
 
 
+def printSystemText(text):
+	print(SYSTEM_COLOR + text + END_COLOR)	
+
+	
+def printMagiText(text):
+	print(MAGI_COLOR + text + END_COLOR)
+
+	
+def userInput():
+	prompt = input(USER_COLOR + "\n$ ")
+	
+	return prompt		
+
+
 # Main logic
+
 context = []
 
 with open(MOD_FILE_PATH) as modFile:
 	mod = modFile.read().strip()
-	print(mod)
+	printSystemText("\nMod: " + mod)
+
+printMagiText("\nMAGI: Greetings")
 
 while True:
-	prompt = input("$ ")
+	prompt = userInput()
 	response = send_prompt(mod, prompt, context)
-	print(response)	
+	printMagiText("\nMAGI: " + response)	
 	
 
