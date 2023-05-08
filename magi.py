85c129ffcaf838139ab1896b6e3f13bea2b5cd70
Kenshiro <>
2023-05-08 21:56:26 +0200

    Summarize internet search

--- magi.py
+++ magi.py
@@ -10,6 +10,8 @@ Description : Autonomous agent
 
 import openai
 import os
+import copy
+from plugins import web
 
 MODEL = "gpt-4"
 CONTEXT_SIZE = 20 # Number of messages to remember
@@ -26,6 +28,11 @@ MISSION_MODE_DISABLED_TEXT = "\nMission mode disabled"
 MODEL_TEXT = "\nModel: "
 GENERATE_WEB_QUERY_TEXT = "Generate a query for google search to get information about this question, write only the query: "
 BROWSE_INTERNET_QUERY_TEXT = "Tell me if you would need updated information from internet to do this task, write only YES or NO: "
+WEB_SEARCH_TEXT = "\n[WEB SEARCH] "
+WEB_SEARCH_LIMIT = 5 # Number of web pages per search
+WEB_SEARCH_SUMMARIZE_TEXT = "Summarize this text and merge it with previous summaries: "
+
+MODEL_ERROR = "[ERROR] An exception occurred while trying to get a response from the model: "
 
 SYSTEM_COLOR = "\033[32m"
 MAGI_COLOR = "\033[99m"
@@ -53,13 +60,18 @@ def split_text_in_blocks(text):
 
 
 def get_completion_from_messages(messages, model = MODEL, temperature = TEMPERATURE):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature, # this is the degree of randomness of the model's output
-    )
+	try:
+		response = openai.ChatCompletion.create(
+		    model=model,
+		    messages=messages,
+		    temperature=temperature, # this is the degree of randomness of the model's output
+		)
 
-    return response.choices[0].message["content"]
+		return response.choices[0].message["content"]
+		
+	except Exception as e:
+		printSystemText(MODEL_ERROR + e, False) 		
+		return ""		
 
 
 def send_prompt(primeDirectives, prompt, context):
@@ -80,16 +92,30 @@ def send_prompt(primeDirectives, prompt, context):
 	return response
 
 
-def printSystemText(text):
-	print(END_COLOR + SYSTEM_COLOR + text + END_COLOR)	
+def printSystemText(text, missionMode):
+	print(END_COLOR + SYSTEM_COLOR + text + END_COLOR)
+	
+	if missionMode == True:
+		saveMissionData(text)	
 
 	
-def printMagiText(text):
+def printMagiText(text, missionMode):
 	print(END_COLOR + MAGI_COLOR + text + END_COLOR)
+	
+	if missionMode == True:
+		saveMissionData(text)		
+
+
+def saveMissionData(text):
+	with open(MISSION_FILE_PATH, 'a') as missionFile:
+		missionFile.write(text + "\n")
 
 	
-def userInput():
+def userInput(missionMode):
 	prompt = input(USER_COLOR + "\n$ ")
+	
+	if missionMode == True:
+		saveMissionData(prompt)	
 	
 	return prompt		
 
@@ -105,70 +131,113 @@ def runMission(primeDirectives, prompt, context):
 	missionTitle = NEW_MISSION_TEXT + mission + "\n\n"
 	
 	printSystemText(missionTitle)
-	saveMissionData(missionTitle)
 	
-	# Start new context
-	context = []
-	
-	# Remember user prompt
-	context.append({'role':'user', 'content':f"{prompt}"})	
-
 	# Remove blank lines
 	mission = '\n'.join(line for line in mission.splitlines() if line.strip())	
 
 	for task in mission.split('\n'):
-		systemText = "\n" + task 
-		printSystemText(systemText)
-		response = send_prompt(primeDirectives, task, context)		
-		magiText = "\n" + response
-		printMagiText(magiText)
-		saveMissionData(systemText + "\n" + magiText)
+		printSystemText("\n" + task, True)
+		response = runPrompt(primeDirectives, task, context, True)		
+		printMagiText("\n" + response, True)
 		
+
+def webSearch(primeDirectives, prompt, webContext, missionMode):
+	summary = ""
+
+	query = send_prompt(primeDirectives, GENERATE_WEB_QUERY_TEXT + prompt, webContext)
+
+	printSystemText(WEB_SEARCH_TEXT + query + "\n", missionMode)
+		
+	urls = web.search(query, WEB_SEARCH_LIMIT)
+
+	for url in urls:
+		printSystemText(url, missionMode)	
+
+	for url in urls:
+		scrapedText = web.scrape(url)
+		blockArray = split_text_in_blocks(scrapedText)
+
+		for block in blockArray:
+			summary = send_prompt(primeDirectives, WEB_SEARCH_SUMMARIZE_TEXT + block, webContext)
+			
+	return summary
+	
+
+def isWebBrowsingRequired(primeDirectives, prompt, webSearchContext):
+	answer = False
+
+	response = send_prompt(primeDirectives, BROWSE_INTERNET_QUERY_TEXT + prompt, webSearchContext)
+	
+	# Remove dots and convert to uppercase
+	response = response.replace(".", "").upper()
+	
+	if response == "YES":
+		answer = True
+		
+	return answer
+	
 	
 def runPrompt(primeDirectives, prompt, context, missionMode):	
+	webSearchContext = copy.deepcopy(context)
+	
+	newPrompt = prompt
+
+	browseWeb = isWebBrowsingRequired(primeDirectives, prompt, webSearchContext)
+	
+	while browseWeb == True:
+		summary = webSearch(primeDirectives, prompt, webSearchContext, missionMode)
+
+		printSystemText("\n" + summary, missionMode)
+
+		newPrompt = prompt + ". " + summary
+			
+		browseWeb = isWebBrowsingRequired(primeDirectives, newPrompt, webSearchContext)			
+
+	# Send the prompt to the model	
+	response = send_prompt(primeDirectives, newPrompt, context)
+
+	printMagiText("\n" + response, missionMode)
+	
+
+def checkPrompt(primeDirectives, prompt, context, missionMode):	
 	if missionMode:
 		runMission(primeDirectives, prompt, context)
 	else:
-		response = send_prompt(primeDirectives, prompt, context)
-		printMagiText("\n" + response)	
+		runPrompt(primeDirectives, prompt, context, missionMode)
 
 
 def switchMissionMode(missionMode):
 	missionMode = not missionMode
 
 	if missionMode == True:
-		printSystemText("\nMission mode enabled")
+		printSystemText("\nMission mode enabled", False)
 	else:
-		printSystemText("\nMission mode disabled")
+		printSystemText("\nMission mode disabled", False)
 		
 	return missionMode
 
-
-def saveMissionData(text):
-    with open(MISSION_FILE_PATH, 'a') as missionFile:
-        missionFile.write(text + "\n")
 
 # Main logic
 context = []
 missionMode = False
 
-printSystemText(MODEL_TEXT + MODEL)
+printSystemText(MODEL_TEXT + MODEL, missionMode)
 
 with open(PRIME_DIRECTIVES_FILE_PATH) as primeDirectivesFile:
 	primeDirectives = primeDirectivesFile.read().strip()
-	printSystemText(PRIME_DIRECTIVES_TEXT + primeDirectives)
+	printSystemText(PRIME_DIRECTIVES_TEXT + primeDirectives, missionMode)
 
-printSystemText(SYSTEM_HINT_TEXT)
-
+printSystemText(SYSTEM_HINT_TEXT, missionMode)
+		
 # Main loop
 while True:
-	prompt = userInput()
+	prompt = userInput(missionMode)
 	
 	command = prompt.split()[0]
 	
 	if command.upper() == MISSION_COMMAND:
 		missionMode = switchMissionMode(missionMode)
 	else:
-		runPrompt(primeDirectives, prompt, context, missionMode)
+		checkPrompt(primeDirectives, prompt, context, missionMode)
 	
  
