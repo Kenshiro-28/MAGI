495a46e8a3fccbd328532e6aea81395187214b5c
Kenshiro <>
2023-11-02 18:16:01 +0100

    Stable Diffusion plugin: optimized prompts

--- plugins/stable_diffusion/test_stable_diffusion.py
+++ plugins/stable_diffusion/test_stable_diffusion.py
@@ -3,8 +3,8 @@ import stable_diffusion
 model = "digiplay/AbsoluteReality_v1.8.1"
 
 prompt = "giant mecha"
-image_specs = "8k, photo, hyperdetailed, dramatic lighting, realistic textures, vibrant colors"
-negative_prompt = "duplicate, mutilated, mutation, deformed, bad anatomy, bad proportions, gross proportions, unnatural body, unnatural pose, malformed limbs, extra limbs, floating limbs, cloned face, disfigured, robot eyes, asymmetrical eyes, deformed iris, deformed pupils, cross-eyed, squinting, missing arms, bad hands, extra fingers, fused fingers, missing legs, bad feet, extra toes, fused toes, jpeg artifacts, watermark, text, signature"
+image_specs = "8K UHD"
+negative_prompt = "lowres"
 
 image = stable_diffusion.generate_image(prompt, model, image_specs, negative_prompt)
 
